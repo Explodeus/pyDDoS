1dfe69f72574d367f8d94248f4c25f40955e9582
Explodeus <volodya.sokha@gmail.com>
2020-03-18 14:14:38 +0200

    Time signatureas added

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ import os
 import time
 import socket
 import random
+
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.years
